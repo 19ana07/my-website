f1eb24227650d159443891cfa70065dd0d203640
19ana07 <programmer.8367@gmail.com>
2025-09-05 15:04:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const flame = document.getElementById("flame");
+
+// Add a click event
+flame.addEventListener("click", () => {
+  //   alert("🔥 You clicked the flame!");
+  document.getElementById("surprise").classList.remove("hidden");
+});
